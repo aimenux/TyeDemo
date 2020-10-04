b4a820b152ebe9236a507707cba492c8c1b7176c
aimenux <devplusdev@gmail.com>
2020-10-04 10:51:08 +0200

    First commit !

---
+++ Microservice/Frontend/WeatherClient.cs
@@ -0,0 +1,33 @@
+﻿using Backend.Models;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Frontend
+{
+    public class WeatherClient
+    {
+        private readonly string _requestUrl;
+        private readonly HttpClient _client;
+
+        private readonly JsonSerializerOptions _options = new JsonSerializerOptions()
+        {
+            PropertyNameCaseInsensitive = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        };
+
+        public WeatherClient(HttpClient client, IConfiguration configuration)
+        {
+            _client = client;
+            _requestUrl = configuration.GetValue<string>("BackendService:RequestUrl");
+        }
+
+        public async Task<WeatherForecast[]> GetWeatherAsync()
+        {
+            var responseMessage = await _client.GetAsync(_requestUrl);
+            var stream = await responseMessage.Content.ReadAsStreamAsync();
+            return await JsonSerializer.DeserializeAsync<WeatherForecast[]>(stream, _options);
+        }
+    }
+}
